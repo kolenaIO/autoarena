95fead0581fb2f0800f3efced072f63d329906e2
Gordon Hart <gordon.hart2@gmail.com>
2024-09-04 16:12:49 -0400

    Remove unnecessary call to setup logger from __main__

--- autostack/__main__.py
+++ autostack/__main__.py
@@ -1,9 +1,7 @@
 import uvicorn
 
 from autostack.args import get_command_line_args
-from autostack.log import initialize_logger
 
 if __name__ == "__main__":
     args = get_command_line_args()
-    initialize_logger()
     uvicorn.run("autostack.main:app", host="0.0.0.0", port=8899, reload=args.dev)

0e766b55cb4e53bcae52db55a553719d1807c953
Gordon Hart <gordon.hart2@gmail.com>
2024-09-05 14:31:45 -0400

    Update 404 page

--- ui/src/components/PageNotFound.tsx
+++ ui/src/components/PageNotFound.tsx
@@ -1,15 +1,22 @@
-import { Button, Stack, Title } from '@mantine/core';
-import { IconError404 } from '@tabler/icons-react';
+import { Button, Stack, Text } from '@mantine/core';
+import { IconFileUnknown } from '@tabler/icons-react';
 import { useRouteError } from 'react-router-dom';
+import { NonIdealState } from './NonIdealState.tsx';
 
 export function PageNotFound() {
   useRouteError();
   return (
     <Stack justify="center" align="center" h="100vh">
       <Stack justify="center" align="center">
-        <Title order={2}>Page Not Found</Title>
-        <IconError404 size={32} />
-        <Button onClick={() => (window.location.href = '/')}>Return Home</Button>
+        <NonIdealState
+          IconComponent={IconFileUnknown}
+          description={
+            <Stack align="center">
+              <Text>Page Not Found</Text>
+              <Button onClick={() => (window.location.href = '/')}>Return Home</Button>
+            </Stack>
+          }
+        />
       </Stack>
     </Stack>
   );

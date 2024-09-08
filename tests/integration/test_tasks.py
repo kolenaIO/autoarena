174c5a73056abd81a243b0a0b97968c912125825
Gordon Hart <gordon.hart2@gmail.com>
2024-09-08 17:53:31 -0400

    Update setup a little

--- tests/integration/test_tasks.py
+++ tests/integration/test_tasks.py
@@ -8,25 +8,13 @@ from autoarena.service.project import ProjectService
 from autoarena.service.task import TaskService
 from tests.integration.judge.conftest import TEST_JUDGE_MODEL_NAMES
 
-DF_BAD_ANSWER = pd.DataFrame.from_records(
-    [
-        dict(prompt="What is 2+2?", response="100 million"),
-        dict(prompt="Is Python 2 still supported?", response="Of course"),
-        dict(prompt="What country shares the longest land border with the USA?", response="Jamaica"),
-        dict(prompt="Who directed Fantastic Mr. Fox?", response="Christopher Nolan"),
-        dict(prompt="Does the Earth orbit the Sun?", response="Of course not"),
-    ]
-)
-
-DF_GOOD_ANSWER = pd.DataFrame.from_records(
-    [
-        dict(prompt="What is 2+2?", response="4"),
-        dict(prompt="Is Python 2 still supported?", response="Not by PSF"),
-        dict(prompt="What country shares the longest land border with the USA?", response="Canada"),
-        dict(prompt="Who directed Fantastic Mr. Fox?", response="Wes Anderson"),
-        dict(prompt="Does the Earth orbit the Sun?", response="Yes"),
-    ]
-)
+TEST_QUESTIONS = [
+    dict(prompt="What is 2+2?", wrong="100 million", right="4"),
+    dict(prompt="Is Python 2 still supported?", wrong="Of course", right="Not by PSF"),
+    dict(prompt="What country shares the longest land border with the USA?", wrong="Jamaica", right="Canada"),
+    dict(prompt="Who directed Fantastic Mr. Fox?", wrong="Christopher Nolan", right="Wes Anderson"),
+    dict(prompt="Does the Earth orbit the Sun?", wrong="Of course not", right="Yes"),
+]
 
 
 def test__task__auto_judge(with_empty_database: None) -> None:
@@ -40,12 +28,14 @@ def test__task__auto_judge(with_empty_database: None) -> None:
         description="Just for testing",
     )
     JudgeService.create(create_judge_request)  # should be enabled by default
-    model_a_id = ModelService.upload_results(project_id, "good-answers", DF_GOOD_ANSWER).id
-    model_b_id = ModelService.upload_results(project_id, "bad-answers", DF_BAD_ANSWER).id
+    df_good_answer = pd.DataFrame.from_records(TEST_QUESTIONS).rename(columns=dict(right="response"))
+    model_a_id = ModelService.upload_results(project_id, "good-answers", df_good_answer).id
+    df_bad_answer = pd.DataFrame.from_records(TEST_QUESTIONS).rename(columns=dict(wrong="response"))
+    model_b_id = ModelService.upload_results(project_id, "bad-answers", df_bad_answer).id
     TaskService.auto_judge(project_id, model_a_id, "good-answers")
     models = ModelService.get_all(project_id)
     model_a = [m for m in models if m.id == model_a_id][0]
     model_b = [m for m in models if m.id == model_b_id][0]
     assert model_a.elo > model_b.elo
-    assert model_a.votes == len(DF_GOOD_ANSWER)
-    assert model_b.votes == len(DF_GOOD_ANSWER)
+    assert model_a.votes == len(TEST_QUESTIONS)
+    assert model_b.votes == len(TEST_QUESTIONS)

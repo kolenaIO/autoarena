b3fe47f49622f5c3b37be856c8bbb98d18e1fc19
Gordon Hart <gordonhart@users.noreply.github.com>
2024-09-10 17:12:48 -0400

    Use remark-breaks plugin to better handle newlines (#49)

--- ui/src/components/MarkdownContent.tsx
+++ ui/src/components/MarkdownContent.tsx
@@ -1,9 +1,10 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
 import rehypeRaw from 'rehype-raw';
 import styles from './MarkdownContent.module.css';
 
-const REMARK_PLUGINS = [remarkGfm];
+const REMARK_PLUGINS = [remarkGfm, remarkBreaks];
 const REHYPE_PLUGINS = [rehypeRaw];
 
 export function MarkdownContent({ children }: { children: string | null | undefined }) {

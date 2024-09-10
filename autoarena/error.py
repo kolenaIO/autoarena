81f2712b3b20e047456b677cc83b7d643e0a2911
Gordon Hart <gordon.hart2@gmail.com>
2024-09-10 10:26:44 -0400

    Golf to improve coverage

--- autoarena/error.py
+++ autoarena/error.py
@@ -1,11 +1,6 @@
 from fastapi import HTTPException
 
 
-class BadRequest(HTTPException):
-    def __init__(self, msg: str):
-        super().__init__(status_code=400, detail=msg)
-
-
 class NotFoundError(HTTPException):
     def __init__(self, msg: str):
         super().__init__(status_code=404, detail=msg)

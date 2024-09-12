d7475eceea7b8782dfb53a7d9a7e5a316b829f1c
Gordon Hart <gordon.hart2@gmail.com>
2024-09-12 16:46:56 -0400

    Add retries to test_judge integration tests to deal with flaky APIs

--- tests/integration/judge/test_judge.py
+++ tests/integration/judge/test_judge.py
@@ -2,7 +2,7 @@ import pytest
 
 from autoarena.api import api
 from autoarena.judge.factory import judge_factory
-from autoarena.judge.wrapper import cleaning_wrapper
+from autoarena.judge.wrapper import cleaning_wrapper, retrying_wrapper
 from tests.integration.judge.conftest import api_judge, TEST_JUDGE_MODEL_NAMES
 
 
@@ -20,7 +20,7 @@ from tests.integration.judge.conftest import api_judge, TEST_JUDGE_MODEL_NAMES
 )
 def test__judge__automated(judge_type: api.JudgeType) -> None:
     model_name = TEST_JUDGE_MODEL_NAMES[judge_type]
-    judge_instance = judge_factory(api_judge(judge_type, model_name), wrappers=[cleaning_wrapper])
+    judge_instance = judge_factory(api_judge(judge_type, model_name), wrappers=[retrying_wrapper, cleaning_wrapper])
     assert judge_instance.judge("What is 2+2?", "4", "100 million") == "A"
     assert judge_instance.n_calls == 1
     assert judge_instance.total_input_tokens > 0

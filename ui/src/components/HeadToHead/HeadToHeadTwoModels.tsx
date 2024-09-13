ca1751339af380e3856d1e0557d704fbdd77f8f1
Gordon Hart <gordon.hart2@gmail.com>
2024-09-13 12:00:42 -0400

    Rework component hierarchy

--- ui/src/components/HeadToHead/HeadToHeadTwoModels.tsx
+++ ui/src/components/HeadToHead/HeadToHeadTwoModels.tsx
@@ -92,121 +92,132 @@ export function HeadToHeadTwoModels({ modelAId, modelBId }: Props) {
   }, [showVoteHistory, headToHead]);
 
   const iconProps = { size: 18 };
-  return !isLoading && nHeadToHeads === 0 ? (
-    <NonIdealState IconComponent={IconCactus} description="No head-to-head matchups between selected models" />
-  ) : !isLoading && headToHeadIndex > nHeadToHeads - 1 ? (
-    <NonIdealState
-      IconComponent={IconBalloon}
-      description={
-        <Stack>
-          <Text>Judged all {nHeadToHeads.toLocaleString()} head-to-head matchups between selected models</Text>
-          <Button onClick={() => navigate(`/project/${projectSlug}`)}>View Leaderboard</Button>
-        </Stack>
-      }
-    />
-  ) : !isLoading ? (
-    <>
-      <Stack pb={height + 32}>
-        <Group justify="space-between">
-          <Checkbox
-            checked={showOnlyJudged}
-            onChange={toggleShowOnlyJudged}
-            label={
-              <Text inherit c="dimmed">
-                Only show head-to-heads with votes
-              </Text>
-            }
-          />
-          <Text c="dimmed" size="sm" fs="italic">
-            {showOnlyJudged
-              ? `${pluralize(nHeadToHeads, 'head-to-head')} with votes between selected models (${nHeadToHeadsTotal.toLocaleString()} total)`
-              : `${pluralize(nHeadToHeads, 'head-to-head')} between selected models`}
-          </Text>
-        </Group>
-        <Paper withBorder p="md" bg="gray.0" style={{ overflow: 'auto' }}>
-          <MarkdownContent>{`**Prompt:** ${headToHead?.prompt}`}</MarkdownContent>
-        </Paper>
-        <SimpleGrid cols={2}>
-          <Paper withBorder p="md" flex={1} style={{ overflow: 'auto' }}>
-            <MarkdownContent>{`**Response A:**\n\n${headToHead?.response_a}`}</MarkdownContent>
-          </Paper>
-          <Paper withBorder p="md" flex={1} style={{ overflow: 'auto' }}>
-            <MarkdownContent>{`**Response B:**\n\n${headToHead?.response_b}`}</MarkdownContent>
-          </Paper>
-        </SimpleGrid>
-      </Stack>
+  return (
+    <Stack pb={height + 32}>
+      <Group justify="space-between">
+        <Checkbox
+          checked={showOnlyJudged}
+          onChange={toggleShowOnlyJudged}
+          label={
+            <Text inherit c="dimmed">
+              Only show head-to-heads with votes
+            </Text>
+          }
+        />
+        <Text c="dimmed" size="sm" fs="italic">
+          {showOnlyJudged
+            ? `${pluralize(nHeadToHeads, 'head-to-head')} with votes between selected models (${nHeadToHeadsTotal.toLocaleString()} total)`
+            : `${pluralize(nHeadToHeads, 'head-to-head')} between selected models`}
+        </Text>
+      </Group>
 
-      <ControlBar ref={controlBarRef}>
-        <Stack align="center" gap="xs">
-          <Text fw="bold">Which response is better?</Text>
-          <SimpleGrid cols={5} spacing="xs">
-            <Button
-              leftSection={<Kbd>p</Kbd>}
-              variant="subtle"
-              color="gray"
-              onClick={navigatePrevious}
-              disabled={headToHeadIndex < 1}
-              h="100%"
-            >
-              Previous
-            </Button>
-            <Button leftSection={<IconArrowLeft {...iconProps} />} onClick={submitVote('A')} h="100%">
-              Left is Better
-            </Button>
-            <Stack gap={4}>
-              <Button size="compact-xs" leftSection={<IconArrowUp {...iconProps} />} onClick={submitVote('-')}>
-                Both are Good
-              </Button>
-              <Button size="compact-xs" leftSection={<IconArrowDown {...iconProps} />} onClick={submitVote('-')}>
-                Both are Bad
-              </Button>
+      {!isLoading && nHeadToHeads === 0 ? (
+        <NonIdealState
+          IconComponent={IconCactus}
+          description={
+            <Stack align="center" gap="xs">
+              <Text inherit>No head-to-heads {showOnlyJudged && 'with votes '}between selected models</Text>
+              {showOnlyJudged && <Text>({pluralize(nHeadToHeadsTotal, 'total head-to-head')})</Text>}
             </Stack>
-            <Button rightSection={<IconArrowRight {...iconProps} />} onClick={submitVote('B')} h="100%">
-              Right is Better
-            </Button>
-            <Button
-              rightSection={<Kbd>n</Kbd>}
-              variant="subtle"
-              color="gray"
-              onClick={navigateNext}
-              disabled={headToHeadIndex >= nHeadToHeads - 1}
-              h="100%"
-            >
-              Next
-            </Button>
-            {showVoteHistory && (
-              <>
-                <div />
-                {[votesA, votesTie, votesB].map((votes, i) => (
-                  <Stack key={i} gap="xs" align="center" fz="xs">
-                    {votes.map((judge, i) => (
-                      <Text key={i} span inherit>
-                        {judge}
-                      </Text>
-                    ))}
-                  </Stack>
-                ))}
-                <div />
-              </>
-            )}
+          }
+        />
+      ) : !isLoading && headToHeadIndex > nHeadToHeads - 1 ? (
+        <NonIdealState
+          IconComponent={IconBalloon}
+          description={
+            <Stack>
+              <Text>Judged all {nHeadToHeads.toLocaleString()} head-to-head matchups between selected models</Text>
+              <Button onClick={() => navigate(`/project/${projectSlug}`)}>View Leaderboard</Button>
+            </Stack>
+          }
+        />
+      ) : !isLoading ? (
+        <>
+          <Paper withBorder p="md" bg="gray.0" style={{ overflow: 'auto' }}>
+            <MarkdownContent>{`**Prompt:** ${headToHead?.prompt}`}</MarkdownContent>
+          </Paper>
+          <SimpleGrid cols={2}>
+            <Paper withBorder p="md" flex={1} style={{ overflow: 'auto' }}>
+              <MarkdownContent>{`**Response A:**\n\n${headToHead?.response_a}`}</MarkdownContent>
+            </Paper>
+            <Paper withBorder p="md" flex={1} style={{ overflow: 'auto' }}>
+              <MarkdownContent>{`**Response B:**\n\n${headToHead?.response_b}`}</MarkdownContent>
+            </Paper>
           </SimpleGrid>
-        </Stack>
 
-        <Box p="md" style={{ position: 'fixed', bottom: 0, right: 0 }}>
-          <Button
-            variant="subtle"
-            color="gray"
-            size="xs"
-            onClick={toggleShowVoteHistory}
-            disabled={!hasVoteHistory}
-            rightSection={<Kbd size="xs">h</Kbd>}
-          >
-            {!hasVoteHistory ? 'No' : showVoteHistory ? 'Hide' : 'Show'} Vote History
-          </Button>
-        </Box>
-      </ControlBar>
-    </>
-  ) : (
-    <></>
+          <ControlBar ref={controlBarRef}>
+            <Stack align="center" gap="xs">
+              <Text fw="bold">Which response is better?</Text>
+              <SimpleGrid cols={5} spacing="xs">
+                <Button
+                  leftSection={<Kbd>p</Kbd>}
+                  variant="subtle"
+                  color="gray"
+                  onClick={navigatePrevious}
+                  disabled={headToHeadIndex < 1}
+                  h="100%"
+                >
+                  Previous
+                </Button>
+                <Button leftSection={<IconArrowLeft {...iconProps} />} onClick={submitVote('A')} h="100%">
+                  Left is Better
+                </Button>
+                <Stack gap={4}>
+                  <Button size="compact-xs" leftSection={<IconArrowUp {...iconProps} />} onClick={submitVote('-')}>
+                    Both are Good
+                  </Button>
+                  <Button size="compact-xs" leftSection={<IconArrowDown {...iconProps} />} onClick={submitVote('-')}>
+                    Both are Bad
+                  </Button>
+                </Stack>
+                <Button rightSection={<IconArrowRight {...iconProps} />} onClick={submitVote('B')} h="100%">
+                  Right is Better
+                </Button>
+                <Button
+                  rightSection={<Kbd>n</Kbd>}
+                  variant="subtle"
+                  color="gray"
+                  onClick={navigateNext}
+                  disabled={headToHeadIndex >= nHeadToHeads - 1}
+                  h="100%"
+                >
+                  Next
+                </Button>
+                {showVoteHistory && (
+                  <>
+                    <div />
+                    {[votesA, votesTie, votesB].map((votes, i) => (
+                      <Stack key={i} gap="xs" align="center" fz="xs">
+                        {votes.map((judge, i) => (
+                          <Text key={i} span inherit>
+                            {judge}
+                          </Text>
+                        ))}
+                      </Stack>
+                    ))}
+                    <div />
+                  </>
+                )}
+              </SimpleGrid>
+            </Stack>
+
+            <Box p="md" style={{ position: 'fixed', bottom: 0, right: 0 }}>
+              <Button
+                variant="subtle"
+                color="gray"
+                size="xs"
+                onClick={toggleShowVoteHistory}
+                disabled={!hasVoteHistory}
+                rightSection={<Kbd size="xs">h</Kbd>}
+              >
+                {!hasVoteHistory ? 'No' : showVoteHistory ? 'Hide' : 'Show'} Vote History
+              </Button>
+            </Box>
+          </ControlBar>
+        </>
+      ) : (
+        <></>
+      )}
+    </Stack>
   );
 }

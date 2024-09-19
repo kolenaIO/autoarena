32b9a4af88f0ad94ee5b98d4a16007269dc30558
Gordon Hart <gordon.hart2@gmail.com>
2024-09-19 14:14:11 -0400

    Try adding additional context manager to as_sse_stream

--- autoarena/api/utils.py
+++ autoarena/api/utils.py
@@ -3,13 +3,16 @@ from typing import TypeVar, AsyncIterator
 from pydantic import RootModel
 from starlette.responses import StreamingResponse
 
+from autoarena.store.database import data_directory, get_data_directory
+
 T = TypeVar("T")  # should be a Pydantic dataclass
 
 
 async def as_sse_stream(object_stream: AsyncIterator[T]) -> AsyncIterator[str]:
-    async for obj in object_stream:
-        obj_json = RootModel[T](obj).model_dump_json()
-        yield f"data: {obj_json}\n\n"
+    with data_directory(get_data_directory()):
+        async for obj in object_stream:
+            obj_json = RootModel[T](obj).model_dump_json()
+            yield f"data: {obj_json}\n\n"
 
 
 # TODO: figure out a way to gracefully terminate this when the server is exiting

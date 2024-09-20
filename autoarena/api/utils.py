022cc95555dd7c01ae0fea30ad47ebbe0fb9781e
Gordon Hart <gordon.hart2@gmail.com>
2024-09-20 09:37:01 -0400

    Capture data directory in execution context for background tasks

--- autoarena/api/utils.py
+++ autoarena/api/utils.py
@@ -1,26 +1,18 @@
+import sys
 from io import StringIO
-from typing import TypeVar, AsyncIterator
+from typing import TypeVar, AsyncIterator, Callable
+
+if sys.version_info[:2] >= (3, 10):
+    from typing import ParamSpec
+else:
+    from typing_extensions import ParamSpec
 
 import pandas as pd
+from fastapi import BackgroundTasks
 from pydantic import RootModel
 from starlette.responses import StreamingResponse
 
 from autoarena.store.database import data_directory, get_data_directory
-
-T = TypeVar("T")  # should be a Pydantic dataclass
-
-
-async def as_sse_stream(object_stream: AsyncIterator[T]) -> AsyncIterator[str]:
-    with data_directory(get_data_directory()):
-        async for obj in object_stream:
-            obj_json = RootModel[T](obj).model_dump_json()
-            yield f"data: {obj_json}\n\n"
-
-
-# TODO: figure out a way to gracefully terminate this when the server is exiting
-class SSEStreamingResponse(StreamingResponse):
-    def __init__(self, object_stream: AsyncIterator[T]):
-        super().__init__(as_sse_stream(object_stream), media_type="text/event-stream")
 
 
 def download_csv_response(df: pd.DataFrame, stem: str) -> StreamingResponse:
@@ -31,3 +23,47 @@ def download_csv_response(df: pd.DataFrame, stem: str) -> StreamingResponse:
         media_type="text/csv",
         headers={"Content-Disposition": f'attachment; filename="{stem}.csv"'},
     )
+
+
+TDataclass = TypeVar("TDataclass")  # should be a Pydantic dataclass
+
+
+def as_sse_stream(object_stream: AsyncIterator[TDataclass]) -> AsyncIterator[str]:
+    # NOTE: this special handling ensures that the data directory in endpoint handler context is captured, as a returned
+    #  StreamingResponse isn't actually read until _after_ the endpoint returns, which is a problem if the stream relies
+    #  on the data directory in context (e.g. by repeatedly reading from the project database)
+    data_dir = get_data_directory()
+
+    async def stream() -> AsyncIterator[str]:
+        with data_directory(data_dir):
+            async for obj in object_stream:
+                obj_json = RootModel[TDataclass](obj).model_dump_json()
+                yield f"data: {obj_json}\n\n"
+
+    return stream()
+
+
+# TODO: figure out a way to gracefully terminate this when the server is exiting
+class SSEStreamingResponse(StreamingResponse):
+    def __init__(self, object_stream: AsyncIterator[TDataclass]):
+        super().__init__(as_sse_stream(object_stream), media_type="text/event-stream")
+
+
+Params = ParamSpec("Params")
+ReturnType = TypeVar("ReturnType")
+
+
+def schedule_background_task(
+    background_tasks: BackgroundTasks,
+    task: Callable[Params, ReturnType],
+    *args: Params.args,
+    **kwargs: Params.kwargs,
+) -> None:
+    """Schedule a background task with the correct data directory captured in execution context."""
+    data_dir = get_data_directory()
+
+    def task_inner(*a: Params.args, **k: Params.kwargs) -> ReturnType:
+        with data_directory(data_dir):
+            return task(*a, **k)
+
+    background_tasks.add_task(task_inner, *args, **kwargs)

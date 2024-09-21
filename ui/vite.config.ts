1644cb91e3753f48441c0e62a1b03bd256705d42
Gordon Hart <gordon.hart2@gmail.com>
2024-09-21 08:17:09 -0400

    Add vite-dts plugin to generate *.d.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true }),
+  ],
   assetsInclude: ['assets/*'],
 });

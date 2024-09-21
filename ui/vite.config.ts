d0035ca3d6d8622b1069b98246417e86693ac944
Gordon Hart <gordon.hart2@gmail.com>
2024-09-21 09:23:22 -0400

    Continue stumbling around config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,33 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ insertTypesEntry: true }),
-  ],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   assetsInclude: ['assets/*'],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'autoarena',
+      // the proper extensions will be added
+      fileName: 'autoarena',
+    },
+    /*
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+     */
+  },
 });

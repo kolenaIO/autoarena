f0fcf595ec22654c7510f8975a15f5181811176f
Gordon Hart <gordon.hart2@gmail.com>
2024-09-21 19:55:26 -0400

    Remove auth definition

--- ui/src/lib/auth.ts
+++
@@ -1,5 +0,0 @@
-export const AUTH0 = {
-  DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN ?? '',
-  CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID ?? '',
-  API_AUDIENCE: import.meta.env.VITE_AUTH0_API_AUDIENCE ?? '',
-};

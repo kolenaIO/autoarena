6d0816e73d4e4c82903e592f26e1102b6df583d6
Gordon Hart <gordon.hart2@gmail.com>
2024-10-02 11:50:41 -0400

    Add prop override pattern to AppConfig

--- ui/src/lib/appConfig.ts
+++ ui/src/lib/appConfig.ts
@@ -1,14 +1,16 @@
 import { fetchEventSource, FetchEventSourceInit } from '@microsoft/fetch-event-source';
-import { Context, createContext, ReactNode, useContext } from 'react';
-import { JudgeType } from '../components';
+import { Context, createContext, useContext } from 'react';
+import { Judges, MainMenu } from '../components';
+
+type PropOverrideKey = React.JSXElementConstructor<any>;
+type PropOverrideValue = { [key: string]: any };
 
 export type AppConfig = {
   baseApiUrl: string;
   basePath: string;
   apiFetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
   apiFetchEventSource: (input: RequestInfo, init: FetchEventSourceInit) => Promise<void>;
-  enabledJudges: JudgeType[];
-  menuExtras: ReactNode[];
+  propOverrides: Map<PropOverrideKey, PropOverrideValue>;
 };
 
 export const DEFAULT_APP_CONFIG: AppConfig = {
@@ -16,12 +18,20 @@ export const DEFAULT_APP_CONFIG: AppConfig = {
   basePath: '',
   apiFetch: fetch,
   apiFetchEventSource: fetchEventSource,
-  enabledJudges: ['openai', 'anthropic', 'cohere', 'ollama', 'gemini', 'together', 'bedrock'],
-  menuExtras: [],
+  propOverrides: new Map([
+    [MainMenu, { extraMenuItems: ['foobar'] }],
+    [Judges, { enabledJudges: ['openai'] }],
+  ] as [PropOverrideKey, PropOverrideValue][]),
 };
 
 export const AppConfigContext: Context<AppConfig> = createContext(DEFAULT_APP_CONFIG);
 
 export function useAppConfig() {
   return useContext(AppConfigContext);
+}
+
+export function usePropOverrides<T>(Component: React.JSXElementConstructor<T>, defaultProps: T) {
+  const { propOverrides } = useAppConfig();
+  const overriddenProps = propOverrides.has(Component) ? propOverrides.get(Component) : {};
+  return { ...defaultProps, ...overriddenProps };
 }

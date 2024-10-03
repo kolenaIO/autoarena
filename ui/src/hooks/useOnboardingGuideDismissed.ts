bb3f76ac27df61f53d795a3cf4372c2c1733346c
Gordon Hart <gordon.hart2@gmail.com>
2024-10-03 09:17:54 -0400

    Improve useOnboardingGuideDismissed hook

--- ui/src/hooks/useOnboardingGuideDismissed.ts
+++ ui/src/hooks/useOnboardingGuideDismissed.ts
@@ -1,10 +1,22 @@
-import { useLocalStorage } from '@mantine/hooks';
+import { readLocalStorageValue, useLocalStorage } from '@mantine/hooks';
+import { useEffect } from 'react';
 
 export function useOnboardingGuideDismissed(projectSlug: string | undefined) {
-  return useLocalStorage<boolean>({
-    key: `/project/${projectSlug}/onboarding-guide-dismissed`,
+  const key = `/project/${projectSlug}/onboarding-guide-dismissed`;
+  const deserialize = (value: string | undefined) => value === 'true';
+
+  const out = useLocalStorage<boolean>({
+    key,
     defaultValue: false,
-    deserialize: value => value === 'true',
+    deserialize,
     serialize: value => (value ? 'true' : 'false'),
   });
+
+  // manually reset value when key changes; useLocalStorage does not handle this itself
+  useEffect(() => {
+    const [, setValue] = out;
+    setValue(readLocalStorageValue({ key, defaultValue: false, deserialize }));
+  }, [key]);
+
+  return out;
 }

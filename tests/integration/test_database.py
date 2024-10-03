95a183779bf4aa0276f414b10d7dafe518bdcc7e
Gordon Hart <gordon.hart2@gmail.com>
2024-10-03 13:24:55 -0400

    Add concurrent write tests

--- tests/integration/test_database.py
+++ tests/integration/test_database.py
@@ -24,3 +24,32 @@ def test__concurrent__read(n_readers: int, test_data_directory: Path) -> None:
 
     assert all(f.result() == [(1, "test")] for f in futures)
     assert time.time() - t0 < 5  # should be relatively fast; no blocking or waiting
+
+
+# up to 32 writers, 512 readers
+@pytest.mark.parametrize("n_writers, n_readers", [(2**i, 2 ** (i + 4)) for i in range(1, 6)])
+def test__concurrent__write(n_writers: int, n_readers: int, test_data_directory: Path) -> None:
+    database_file = test_data_directory / "test__concurrent__write.sqlite"
+    with get_database_connection(database_file, commit=True) as conn:
+        conn.execute("CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)")
+        conn.execute("INSERT INTO test (value) VALUES ('test')")
+
+    def read() -> list[tuple[str]]:
+        with get_database_connection(database_file) as conn_reader:
+            return conn_reader.cursor().execute("SELECT value FROM test WHERE id = 1").fetchall()
+
+    def write() -> None:
+        with get_database_connection(database_file, commit=True) as conn_writer:
+            conn_writer.cursor().execute("INSERT INTO test (value) VALUES ('concurrent')")
+
+    t0 = time.time()
+    with ThreadPoolExecutor(max_workers=n_writers + n_readers) as executor:
+        readers = [executor.submit(read) for _ in range(n_readers)]
+        writers = [executor.submit(write) for _ in range(n_writers)]
+
+    assert all(f.result() == [("test",)] for f in readers)
+    assert all(f.result() is None for f in writers)
+    assert time.time() - t0 < 5  # should be relatively fast; not much waiting
+
+    with get_database_connection(database_file) as conn:
+        assert conn.cursor().execute("SELECT COUNT(*) FROM test").fetchone() == (1 + n_writers,)
